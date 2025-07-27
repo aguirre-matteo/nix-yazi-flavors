ecdd2242571215ddc3aad6b932d694f6b41e1e98
Aguirre Matteo <aguirre-matteo@proton.me>
2025-07-27 15:41:00 -0300

    misc: change flake structure and add checks

--- flake.nix
+++ flake.nix
@@ -4,7 +4,16 @@
     Ready to be used with Home-Manager's Yazi module.
   '';
 
-  outputs = { self, ... }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, nixpkgs, systems }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = eachSystem (system: import ./packages nixpkgs.legacyPackages.${system});
+    checks = eachSystem(system: self.packages.${system});
     overlay = import ./overlay.nix;
   };
 }
---
+++ packages/default.nix
@@ -0,0 +1,23 @@
+pkgs: {
+  catppuccin-frappe = pkgs.callPackage ./catppuccin-frappe.nix {};
+  catppuccin-latte = pkgs.callPackage ./catppuccin-latte.nix {};
+  catppuccin-macchiato = pkgs.callPackage ./catppuccin-macchiato.nix {};
+  catppuccin-mocha = pkgs.callPackage ./catppuccin-mocha.nix {};
+
+  tokyonight-day = pkgs.callPackage ./tokyonight-day.nix {};
+  tokyonight-moon = pkgs.callPackage ./tokyonight-moon.nix {};
+  tokyonight-night = pkgs.callPackage ./tokyonight-night.nix {};
+  tokyonight-storm = pkgs.callPackage ./tokyonight-storm.nix {};
+
+  vscode-dark-modern = pkgs.callPackage ./vscode-dark-modern.nix {};
+  vscode-dark-plus = pkgs.callPackage ./vscode-dark-plus.nix {};
+  vscode-light-modern = pkgs.callPackage ./vscode-light-modern.nix {};
+  vscode-light-plus = pkgs.callPackage ./vscode-light-plus.nix {};
+
+  kanagawa = pkgs.callPackage ./kanagawa.nix {};
+  kanagawa-dragon = pkgs.callPackage ./kanagawa-dragon.nix {};
+
+  dracula = pkgs.callPackage ./dracula.nix {};
+  nord = pkgs.callPackage ./nord.nix {};
+  onedark = pkgs.callPackage ./onedark.nix {};
+}
